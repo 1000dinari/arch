3dc5d9e176ee3c7e265b545a241731bf42d3b0a9
1000dinari <51042294+1000dinari@users.noreply.github.com>
2019-05-26 12:54:28 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syyuu awesome gimp firefox openvpn uget deluge clementine thunderbird irssi ristretto vlc terminator pcmanfm ark git gparted filelight nitrogen geany dmenu fortune-mod qt5-base qt5-svg qt5-declarative qt5-quickcontrols xfce4 xfce4-whiskermenu-plugin
+sudo pacman -Syyuu wget awesome gimp firefox openvpn uget deluge clementine thunderbird irssi ristretto vlc terminator pcmanfm ark git gparted filelight nitrogen geany dmenu fortune-mod qt5-base qt5-svg qt5-declarative qt5-quickcontrols xfce4 xfce4-whiskermenu-plugin
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
